5ae78a28a8565d4ebe4c1e8988cf3c0f260bc4e8
vairamuthu11a <vairamuthu11a@gmail.com>
2024-08-21 13:20:10 +0530

    six commit

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV == 'production') {
     });
 }
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server listening to Port ${process.env.PORT} in ${process.env.NODE_ENV}`);
+app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server listening to Port ${process.env.PORT || 8000} in ${process.env.NODE_ENV}`);
 
 });
